2ab517475fb816f99b1d3ce267a1316d81448afd
Taylor Bonar <32309887+tbonar@users.noreply.github.com>
2022-07-27 19:14:57 -0500

    Fixed to only pull Asset_ID 1 and removed old files

--- data_cleanup.R
+++ data_cleanup.R
@@ -2,14 +2,21 @@
 
 # Read full dataset
 btc.full = read_parquet("data/train.parquet.gzip")
-# Convert to readable timestamps from UNIX
-head(btc.full)
+# Only get BTC asset rows
+btc.full <- btc.full %>% filter(Asset_ID==1)
+summary(btc.full)
+as_datetime(head(btc.full$timestamp, n=1))
+as_datetime(tail(btc.full$timestamp, n=1))
 
 # Create Training Set for 3 month period
 btc.train = subset(btc.full, timestamp > as.POSIXct("2021-02-28 23:59:00", tz="GMT") & timestamp < as.POSIXct("2021-06-01 00:00:00", tz="GMT"))
+as_datetime(head(btc.train$timestamp, n=1))
+as_datetime(tail(btc.train$timestamp, n=1))
 head(btc.train)
 
 btc.short = subset(btc.full, timestamp > as.POSIXct("2021-05-31 23:59:00", tz="GMT") & timestamp < as.POSIXct("2021-06-02 00:00:00", tz="GMT"))
+as_datetime(head(btc.short$timestamp, n=1))
+as_datetime(tail(btc.short$timestamp, n=1))
 head(btc.short)
 
 btc.long = subset(btc.full, timestamp > as.POSIXct("2021-05-31 23:59:00", tz="GMT") & timestamp < as.POSIXct("2021-06-08 00:00:00", tz="GMT"))
